2db3311fe1878ba116832b93508ee848e59f28cb
Ilia Kalygin <58369822+iliakalygin@users.noreply.github.com>
2023-12-29 01:22:04 +0100

    LoginGrid Formular mit Codebehind hinzugefügt. (Hardcoded)

--- JetStreamServiceApp/JetStreamServiceApp/MainWindow.xaml.cs
+++ JetStreamServiceApp/JetStreamServiceApp/MainWindow.xaml.cs
@@ -37,7 +37,7 @@ namespace JetStreamServiceApp
 
 
 
-        // EditStackPanel ----------------------------------------------------------
+        // EditGrid ----------------------------------------------------------
 
 
 
@@ -45,6 +45,24 @@ namespace JetStreamServiceApp
         {
             GridMain.Visibility = Visibility.Visible;
             GridEdit.Visibility = Visibility.Hidden;
+        }
+
+        // LoginGrid----------------------------------------------------------
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            // Überprüfe, ob der Benutzername "admin" und das Passwort "c#" sind
+            if (textBoxUsername.Text == "admin" && passwordBoxPassword.Password == "c#")
+            {
+                // Setze die Sichtbarkeit der Grids entsprechend
+                GridMain.Visibility = Visibility.Visible;
+                GridLogin.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                // Wenn die Anmeldeinformationen falsch sind, könntest du eine Fehlermeldung anzeigen oder andere Aktionen durchführen.
+                MessageBox.Show("Falscher Benutzername oder Passwort. Bitte versuche es erneut.");
+            }
         }
 
     }
